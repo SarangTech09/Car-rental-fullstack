1699fe4e5059a7aad5a471659fb968fdf3893af9
SarangTech09 <sarangtadaskar09@gmail.com>
2026-03-06 22:00:27 +0530

    fix future cors issue

--- server/server.js
+++ server/server.js
@@ -14,7 +14,10 @@ const app = express();
 await connectDB();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+     origin: "https://car-rental-frontend-eta-three.vercel.app",
+     credentials: true
+}));
 app.use(express.json());
 
 // Routes
